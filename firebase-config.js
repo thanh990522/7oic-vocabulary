079629e235e75bb78bd16729972d5a67315bc44e
Hà Chí Thanh <64676978+thanh990522@users.noreply.github.com>
2026-08-18 14:21:41 +0700

    Add firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBLYSGVx1dIs2D4aM1XEWsrrXKJgj67Ib4",
+  authDomain: "oic-vocabulary.firebaseapp.com",
+  projectId: "oic-vocabulary",
+  storageBucket: "oic-vocabulary.firebasestorage.app",
+  messagingSenderId: "1091311367678",
+  appId: "1:1091311367678:web:c9ffcc499e1c925880fcaf",
+  measurementId: "G-RNXDMCRYT9"
+};
+
+export const TEACHER_EMAIL = "hachithanh2251999@gmail.com";
+export const FIREBASE_SDK_VERSION = "12.17.1";
